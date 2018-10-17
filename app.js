000b29484d9f296eb56a1be282dd63df679d5257
Erin Dubuc <erin.dubuc9@gmail.com>
2018-10-17 18:05:58 +0000

    refactored params

--- app.js
+++ app.js
@@ -2,20 +2,50 @@ var express = require('express');
 // calling the express function gives an application instance
 var app = express();
 
-var logger = require('./logger'); 
-// var logger requires the local module with ./ to use it - not an NPM mod
-app.use(logger);
-// add to stack with app.use
-
-app.use(express.static('public'));
-
+// in order to store additional info on blocks - need to make a JS object vs an array
+var blocks = {
+    'Fixed': 'Fastened securely in position',
+    'Movable': 'Capable of being moved',
+    'Rotating': 'Moving in a circle around its center'
+    };
 app.get('/blocks', function(request, response) {
-    var blocks = ['Fixed', 'Movable', 'Rotating'];
-    response.json(blocks);
+    response.json(Object.keys(blocks));
     });
-
+    
+// var locations = {
+//     'Fixed': 'First floor',
+//     'Movable': 'Second floor',
+//     'Rotating': 'Penthouse'
+//     };
+    
+//     app.param('name', function(request, response, next) {
+//         // var description = blocks[request.params.name];
+//         // doing two things - reading request param and looking up the object's properties, so we split it up to improve
+//         var name = request.params.name;
+//         var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+        
+//         request.blockName = block;
+//         // assign block variable to new property on request object
+//         // blockName can be accessed by all routes in app
+//         next();
+//         });
+    
+//     app.get('/blocks/:name', function(request, response) {  
+        
+//         var description = blocks[request.blockName];
+//         // block name is now in the same format as the properties in blocks object
+//         if (!description) {
+//             response.status(404).json('No description found for ' + request.params.name);
+//             } else {
+//               response.json(description);
+//             // response defaults to 200 Success status code  
+//             }
+        
+//         });
+    
+//     app.get('/locations/:name', function(request, response) {
+//         var location = locations[request.blockName];
+//         });
 
 // binds application to tcp port 8080
-app.listen(8080, function() {
-    console.log('Listening on port 8080 \n');
-    });
+app.listen(8080);
